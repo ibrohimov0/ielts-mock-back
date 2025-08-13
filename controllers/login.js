82149c17bc180368e7c5856d432305878734c038
Abdulloh <ibrohimovabdulloh573@gmail.com>
2025-08-13 22:07:39 +0500

    Fix login response handling and error propagation in postLogin method

--- controllers/login.js
+++ controllers/login.js
@@ -10,7 +10,15 @@ exports.postLogin = async(req, res, next) => {
         if(data.length > 0) {
             data.map(async(e) => {
                 const token = await jwt.sign({id: e.id,name: e.name,password: e.password}, process.env.JWT_KEY)
+                res.status(200).json({data: e,token})
             })
+        } else {
+            res.status(404).json({status: 404, message: "Admin Not Found"})
         }
+    }).catch(err => {
+        if(!err.statusCode) {
+            err.statusCode = 500
+        }
+        next(err)
     })
 }
