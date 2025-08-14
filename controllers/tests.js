1b863417f017ff7ca9e2a6fc48671f3f09bca70b
Abdulloh <ibrohimovabdulloh573@gmail.com>
2025-08-14 11:53:38 +0500

    Refactor postTest method to include validation for request body and options

--- controllers/tests.js
+++ controllers/tests.js
@@ -14,8 +14,23 @@ exports.getTests = async (req, res, next) => {
     })
 }
 
-exports.postTest = async(req,res,next) => {
-    return await TestModel.().then(test => {
+exports.postTest = async (req, res, next) => {
+    if (!req.body) {
+        return res.status(400).json({ message: 'Body required!' });
+    }
+
+    const { options } = req.body
+
+    if (!options || options.length !== 4) {
+        return res.status(400).json({ error: 'Options must contain exactly 4 items' });
+    }
+
+    const trueContent = options.filter(opt => opt.isCorrect === true).length
+    if (trueContent !== 1) {
+        return res.status(400).json({ error: 'Exactly 1 option must be correct' });
+    }
+
+    return await TestModel.create(req.body).then(test => {
         res.status(200).json(test)
     }).catch(err => {
         if (!err.statusCode) {
@@ -25,24 +40,24 @@ exports.postTest = async(req,res,next) => {
     })
 }
 
-exports.patchTest = async(req,res,next) => {
-    return await TestModel.().then(test => {
-        res.status(200).json(test)
-    }).catch(err => {
-        if (!err.statusCode) {
-            err.statusCode = 500
-        }
-        next(err)
-    })
-}
+// exports.patchTest = async(req,res,next) => {
+//     return await TestModel.().then(test => {
+//         res.status(200).json(test)
+//     }).catch(err => {
+//         if (!err.statusCode) {
+//             err.statusCode = 500
+//         }
+//         next(err)
+//     })
+// }
 
-exports.deleteTest = async(req,res,next) => {
-    return await TestModel.().then(test => {
-        res.status(200).json(test)
-    }).catch(err => {
-        if (!err.statusCode) {
-            err.statusCode = 500
-        }
-        next(err)
-    })
-}
+// exports.deleteTest = async(req,res,next) => {
+//     return await TestModel.().then(test => {
+//         res.status(200).json(test)
+//     }).catch(err => {
+//         if (!err.statusCode) {
+//             err.statusCode = 500
+//         }
+//         next(err)
+//     })
+// }
