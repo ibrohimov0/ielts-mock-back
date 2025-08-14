87abade7448fcbb16d53adcdae892efcfb0ced12
Abdulloh <ibrohimovabdulloh573@gmail.com>
2025-08-14 12:07:53 +0500

    Refactor postTest method to improve error handling and maintainability

--- controllers/tests.js
+++ controllers/tests.js
@@ -15,49 +15,55 @@ exports.getTests = async (req, res, next) => {
 }
 
 exports.postTest = async (req, res, next) => {
-    if (!req.body) {
-        return res.status(400).json({ message: 'Body required!' });
-    }
+    try {
+        await jwt.verify(req.headers.token, process.env.JWT_KEY)
 
-    const { options } = req.body
-
-    if (!options || options.length !== 4) {
-        return res.status(400).json({ error: 'Options must contain exactly 4 items' });
-    }
-
-    const trueContent = options.filter(opt => opt.isCorrect === true).length
-    if (trueContent !== 1) {
-        return res.status(400).json({ error: 'Exactly 1 option must be correct' });
-    }
-
-    return await TestModel.create(req.body).then(test => {
-        res.status(200).json(test)
-    }).catch(err => {
-        if (!err.statusCode) {
-            err.statusCode = 500
+        if (!req.body) {
+            return res.status(400).json({ message: 'Body required!' });
         }
-        next(err)
-    })
+
+        const { options } = req.body
+
+        if (!options || options.length !== 4) {
+            return res.status(400).json({ error: 'Options must contain exactly 4 items' });
+        }
+
+        const trueContent = options.filter(opt => opt.isCorrect === true).length
+        if (trueContent !== 1) {
+            return res.status(400).json({ error: 'Exactly 1 option must be correct' });
+        }
+
+        return await TestModel.create(req.body).then(test => {
+            res.status(200).json(test)
+        }).catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500
+            }
+            next(err)
+        })
+    } catch {
+        return res.status(403).json({ message: "Not Allowed! Forbidden" })
+    }
 }
 
-// exports.patchTest = async(req,res,next) => {
-//     return await TestModel.().then(test => {
-//         res.status(200).json(test)
-//     }).catch(err => {
-//         if (!err.statusCode) {
-//             err.statusCode = 500
-//         }
-//         next(err)
-//     })
-// }
+exports.patchTest = async(req,res,next) => {
+    // return await TestModel.().then(test => {
+    //     res.status(200).json(test)
+    // }).catch(err => {
+    //     if (!err.statusCode) {
+    //         err.statusCode = 500
+    //     }
+    //     next(err)
+    // })
+}
 
-// exports.deleteTest = async(req,res,next) => {
-//     return await TestModel.().then(test => {
-//         res.status(200).json(test)
-//     }).catch(err => {
-//         if (!err.statusCode) {
-//             err.statusCode = 500
-//         }
-//         next(err)
-//     })
-// }
+exports.deleteTest = async(req,res,next) => {
+    // return await TestModel.().then(test => {
+    //     res.status(200).json(test)
+    // }).catch(err => {
+    //     if (!err.statusCode) {
+    //         err.statusCode = 500
+    //     }
+    //     next(err)
+    // })
+}
